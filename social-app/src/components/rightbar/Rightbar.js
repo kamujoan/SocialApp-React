39348ba395ee1696cafbd5ab8046adadb447df95
Joan Kamu <topibooks@outlook.com>
2021-11-29 17:53:09 +0300

    7th commit

---
+++ social-app/src/components/rightbar/Rightbar.js
@@ -0,0 +1,15 @@
+import './Rightbar.css'
+
+export default function Rightbar() {
+    return (
+      <div className="rightbar">
+        <div className="rightbarWrapper">
+          <img
+            src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/3294d36d0a3a3c5950ee156db55c2cbd_screen.jpg?ts=1638197269"
+            alt=""
+            className="rightbarAd"
+          />
+        </div>
+      </div>
+    );
+}
